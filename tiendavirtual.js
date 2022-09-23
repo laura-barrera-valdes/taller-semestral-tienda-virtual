1cd1636600e7b1afc6ff7e844aa9336583a09c30
laura-barrera-valdes <laurabv99@gmail.com>
2022-09-23 18:43:33 -0500

    inicie la construccion del modal con js

--- tiendavirtual.js
+++ tiendavirtual.js
@@ -5,3 +5,27 @@ var swiper = new Swiper(".mySwiper", {
       clickable: true,
     },
   });
+
+  /* Inicio del Modal */
+
+  var modal = document.getElementById('popup-modal');
+var btn = document.getElementById("open-popup-modal");
+var span = document.getElementsByClassName("modal-close")[0];
+btn.onclick = function() {
+modal.style.display = "block";
+}
+span.onclick = function() {
+modal.style.display = "none";
+}
+window.onload = function() {
+setTimeout(function() {
+modal.style.display = 'block';
+}, 0);
+}
+window.onclick = function(event) {
+if (event.target == modal) {
+modal.style.display = "none";
+}
+}
+
+  /*Final del Modal */
