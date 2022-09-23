5a32d42b1567e7f272397b0771323e5f66240506
laura-barrera-valdes <laurabv99@gmail.com>
2022-09-23 15:18:46 -0500

    termine el swiper con js

--- tiendavirtual.js
+++ tiendavirtual.js
@@ -1,5 +1,5 @@
 var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
+    /* spaceBetween: 30, es para poner espacio entre las imagenes*/
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
