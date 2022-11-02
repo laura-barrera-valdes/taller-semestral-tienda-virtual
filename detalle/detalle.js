6ecff8ebac102de6b5ef67c54b14779f86b85fec
laura-barrera-valdes <laurabv99@gmail.com>
2022-11-02 01:57:44 -0500

    situe, organice y muestre la pantalla de detalles de productos

--- detalle/detalle.js
+++ detalle/detalle.js
@@ -1,1 +1,28 @@
-/*console.log(new URLSearchParams(window.location.search).get('id'));*/
+console.log(new URLSearchParams(window.location.search).get('nombre'));
+console.log(data.id);
+console.log(data);
+
+const nameQueryParam = new URLSearchParams(window.location.search).get('nombre');
+let product;
+searchProduct();
+renderProduct();
+
+function searchProduct() {
+    product = data.find((item) => nameQueryParam === item.nombre);
+    console.log(product);
+};
+
+function renderProduct() {
+    const title = document.getElementById('title');
+    const price = document.getElementById('price');
+    const img = document.getElementById('img');
+    const type = document.getElementById('type');
+    const collection = document.getElementById('collection');
+
+    title.textContent = product.nombre;
+    price.textContent = product.precio;
+    type.textContent = product.tipo_prenda;
+    collection.textContent = product.coleccion;
+
+    img.setAttribute('src', product.ruta_img);
+};
--- ropa/ropa.js
+++ ropa/ropa.js
@@ -3,21 +3,21 @@ displayProducts();
 function displayProducts() {
   const productsSection = document.getElementById('products-container')
 
-  data.forEach(data => {
+  data.forEach(product => {
 
     const card = document.createElement('article')
     card.classList.add('card')
     card.innerHTML = `
     <div class="card-image">
-      <img class="card-inner-image" src="${data.ruta_img}">
+      <img class="card-inner-image" src="${product.ruta_img}">
       <div class="card-title iconos">
         <a href="#!" class="shoppingcartlink"><i class="material-icons iconshoppingcart">shopping_cart</i></a>
         <a href="#!" class="shoppingcartlink"><i class="material-icons iconshoppingcart">favorite</i></a>
       </div>
     </div>
     <div class="card-content">
-      <a href="../detalle/detalle.html?data=${data.nombre}" class="nombre"><span class="card-title">${data.nombre}</span></a>
-      <p><b>$ ${data.precio}</b></p>
+      <a href="../detalle/detalle.html?nombre=${product.nombre}" class="nombre"><span class="card-title">${product.nombre}</span></a>
+      <p><b>$ ${product.precio}</b></p>
     </div>
     `
     productsSection.append(card)
