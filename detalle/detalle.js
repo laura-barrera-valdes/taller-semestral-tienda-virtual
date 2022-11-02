b7e00405b52fd431680d60eaf90b2ccf1baec575
laura-barrera-valdes <laurabv99@gmail.com>
2022-11-02 02:58:04 -0500

    agreguei nteracciÃon de multiples fotos en pagina detalle

--- detalle/detalle.js
+++ detalle/detalle.js
@@ -1,3 +1,11 @@
+let swiper = new Swiper(".mySwiper", {
+    /* spaceBetween: 30, es para poner espacio entre las imagenes*/
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
 console.log(new URLSearchParams(window.location.search).get('nombre'));
 console.log(data.id);
 console.log(data);
@@ -16,6 +24,7 @@ function renderProduct() {
     const title = document.getElementById('title');
     const price = document.getElementById('price');
     const img = document.getElementById('img');
+    const img2 = document.getElementById('img2');
     const type = document.getElementById('type');
     const collection = document.getElementById('collection');
 
@@ -25,4 +34,5 @@ function renderProduct() {
     collection.textContent = product.coleccion;
 
     img.setAttribute('src', product.ruta_img);
+    img2.setAttribute('src', product.ruta_img_hover);
 };
